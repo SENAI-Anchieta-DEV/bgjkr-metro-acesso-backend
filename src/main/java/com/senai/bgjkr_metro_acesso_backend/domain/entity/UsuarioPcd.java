3dd5f9533ab54668ce2e3d108b47ebe1be08bd04
brayanxd4547 <brayanlainescarvalho@gmail.com>
2026-03-09 17:39:54 -0300

    BGJKR-99 feat: regra de negócio - criação de enum TipoDeficiencia para usuário PcD

--- src/main/java/com/senai/bgjkr_metro_acesso_backend/domain/entity/UsuarioPcd.java
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/domain/entity/UsuarioPcd.java
@@ -2,10 +2,11 @@ package com.senai.bgjkr_metro_acesso_backend.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-import jakarta.persistence.CascadeType;
+import com.senai.bgjkr_metro_acesso_backend.domain.enums.TipoDeficiencia;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.OneToOne;
 
 import lombok.AllArgsConstructor;
@@ -21,8 +22,9 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @SuperBuilder
 public class UsuarioPcd extends Usuario {
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private String tipoNecessidade;
+    private TipoDeficiencia tipoDeficiencia;
 
     @Column(nullable = false)
     private boolean desejaSuporte;
---
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/domain/enums/TipoDeficiencia.java
@@ -0,0 +1,7 @@
+package com.senai.bgjkr_metro_acesso_backend.domain.enums;
+
+public enum TipoDeficiencia {
+    VISUAL,
+    AUDITIVO,
+    MOTOR
+}
