36fed58c1b63e7c3e08f805e385a3a17b58d1bac
Brayan Laines Carvalho <brayanlainescarvalho@gmail.com>
2026-03-09 10:39:45 -0300

    BGJKR-49 feat: criação do projeto com Spring Boot

---
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/MetroAcessoApplication.java
@@ -0,0 +1,13 @@
+package com.senai.bgjkr_metro_acesso_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroAcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroAcessoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/bgjkr_metro_acesso_backend/MetroAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.bgjkr_metro_acesso_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
