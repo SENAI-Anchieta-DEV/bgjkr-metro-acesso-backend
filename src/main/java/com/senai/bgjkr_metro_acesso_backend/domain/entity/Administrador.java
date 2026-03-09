54a4b87c7829f9ad57edf37bbda0fd8e170ddcf7
Brayan Laines Carvalho <brayanlainescarvalho@gmail.com>
2026-03-09 11:05:03 -0300

    BGJKR-50 feat: criação das classes básicas para arquitetura de camadas DDD

---
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/domain/entity/Administrador.java
@@ -0,0 +1,4 @@
+package com.senai.bgjkr_metro_acesso_backend.domain.entity;
+
+public class Administrador {
+}
---
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/interface_ui/controller/SensorController.java
@@ -0,0 +1,4 @@
+package com.senai.bgjkr_metro_acesso_backend.interface_ui.controller;
+
+public class SensorController {
+}
