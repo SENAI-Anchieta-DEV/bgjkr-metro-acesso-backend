291deea9d63e809dcf0ba51f158f2af82af19160
brayanxd4547 <brayanlainescarvalho@gmail.com>
2026-03-16 20:47:34 -0300

    BGJKR-94 fix: inclusão de import de EstacaoSummaryDto

--- src/main/java/com/senai/bgjkr_metro_acesso_backend/application/dto/sensor/SensorResponseDto.java
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/application/dto/sensor/SensorResponseDto.java
@@ -1,5 +1,6 @@
 package com.senai.bgjkr_metro_acesso_backend.application.dto.sensor;
 
+import com.senai.bgjkr_metro_acesso_backend.application.dto.estacao.EstacaoSummaryDto;
 import com.senai.bgjkr_metro_acesso_backend.domain.entity.Sensor;
 
 public record SensorResponseDto(
