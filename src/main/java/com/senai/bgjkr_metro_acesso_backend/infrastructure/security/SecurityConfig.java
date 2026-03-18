ff00f609c25b274299e9efcd0df37000a25a59ba
gabriel <gabrielnmyzy@gmail.com>
2026-03-18 18:19:11 -0300

    BGJKR-72 feat: implementação Configuração JWT (application.properties) e SecurityConfig

--- src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/security/SecurityConfig.java
@@ -1,4 +1,27 @@
 package com.senai.bgjkr_metro_acesso_backend.infrastructure.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // Desativamento do CSRF
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/auth/login").permitAll() // Garante que o login não exija token
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
 }
