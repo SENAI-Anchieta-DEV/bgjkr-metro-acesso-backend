58f5da9c0a075f903de99b9ec8ee4e34f55ca07a
gabriel <gabrielnmyzy@gmail.com>
2026-03-26 11:09:36 -0300

    BGJKR-91 feat: inserindo casos de autorização dos endpoints por ROLES.

--- src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.senai.bgjkr_metro_acesso_backend.infrastructure.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,16 +21,45 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
+                .authorizeHttpRequests(auth -> auth
 
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/login").permitAll() // Garante que o login não exija token
+                        .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+
+                        .requestMatchers(HttpMethod.GET, "/api/admin/**").hasRole("ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.POST, "/api/admin/**").hasRole("ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.PUT, "/api/admin/**").hasRole("ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.DELETE, "/api/admin/**").hasRole("ADMINISTRADOR")
+
+                        .requestMatchers(HttpMethod.GET, "/api/agente/**").hasAnyRole("ADMINISTRADOR", "AGENTE_ATENDIMENTO")
+                        .requestMatchers(HttpMethod.POST, "/api/agente/**").hasRole("ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.PUT, "/api/agente/**").hasAnyRole("ADMINISTRADOR", "AGENTE_ATENDIMENTO")
+                        .requestMatchers(HttpMethod.DELETE, "/api/agente/**").hasRole("ADMINISTRADOR")
+
+                        .requestMatchers(HttpMethod.GET, "/api/pcd/**").hasAnyRole("USUARIO_PCD", "ADMINISTRADOR", "AGENTE_ATENDIMENTO")
+                        .requestMatchers(HttpMethod.POST, "/api/pcd/**").hasRole( "ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.PUT, "/api/pcd/**").hasAnyRole("USUARIO_PCD", "ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.DELETE, "/api/pcd/**").hasAnyRole("USUARIO_PCD", "ADMINISTRADOR")
+
+                        .requestMatchers(HttpMethod.GET, "/api/formulario/**").hasAnyRole("USUARIO_PCD", "ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.POST, "/api/formulario/**").hasAnyRole("USUARIO_PCD", "ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.PUT, "/api/formulario/**").hasAnyRole("USUARIO_PCD", "ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.DELETE, "/api/formulario/**").hasAnyRole("USUARIO_PCD", "ADMINISTRADOR")
+
+                        .requestMatchers(HttpMethod.GET, "/api/formulario/**").hasAnyRole("USUARIO_PCD", "ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.POST, "/api/formulario/**").hasAnyRole("USUARIO_PCD", "ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.PUT, "/api/formulario/**").hasAnyRole("USUARIO_PCD", "ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.DELETE, "/api/formulario/**").hasAnyRole("USUARIO_PCD", "ADMINISTRADOR")
+
+                        .requestMatchers(HttpMethod.GET, "/api/estacao/**").hasAnyRole( "ADMINISTRADOR", "AGENTE_ATENDIMENTO")
+                        .requestMatchers(HttpMethod.POST, "/api/estacao/**").hasRole("ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.PUT, "/api/estacao/**").hasRole("ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.DELETE, "/api/estacao/**").hasRole("ADMINISTRADOR")
+
                         .anyRequest().authenticated()
                 )
-
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
