6cfb5fff27eece1632ec37554652077cf7f10ce5
gabriel <gabrielnmyzy@gmail.com>
2026-03-27 10:21:28 -0300

    BGJKR-92 feat: implementação da anotação @Configuration

--- src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/config/MethodSecurityConfig.java
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/config/MethodSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.senai.bgjkr_metro_acesso_backend.infrastructure.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+@Configuration
 @EnableMethodSecurity
 public class MethodSecurityConfig {
 }
