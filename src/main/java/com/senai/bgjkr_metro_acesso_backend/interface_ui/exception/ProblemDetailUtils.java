2768083a97988e6375c0219801dc42245f9984a6
gabriel <gabrielnmyzy@gmail.com>
2026-04-06 11:16:01 -0300

    BGJKR-70 feat: padronizacao campos essenciais do ProblemDetail

--- src/main/java/com/senai/bgjkr_metro_acesso_backend/interface_ui/exception/ProblemDetailUtils.java
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/interface_ui/exception/ProblemDetailUtils.java
@@ -1,4 +1,17 @@
 package com.senai.bgjkr_metro_acesso_backend.interface_ui.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+import java.net.URI;
+
 public class ProblemDetailUtils {
+
+    public static ProblemDetail buildProblem(HttpStatus status, String title, String detail, String path) {
+        ProblemDetail problem = ProblemDetail.forStatus(status);
+        problem.setTitle(title);
+        problem.setDetail(detail);
+        problem.setInstance(URI.create(path));
+        return problem;
+    }
 }
