21a0fcd6dabf2531b1dfa57c53123e089f4ed58e
Brayan Laines Carvalho <brayanlainescarvalho@gmail.com>
2026-04-07 09:51:31 -0300

    BGJKR-318 feat: finalização do render.yaml, pom.xml e application.properties

--- src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/security/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
