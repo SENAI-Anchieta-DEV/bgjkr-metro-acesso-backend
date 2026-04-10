33f70b63c0004b701d7171651647c17b39d30125
gabriel <gabrielnmyzy@gmail.com>
2026-04-10 09:03:37 -0300

    BGJKR-315 feat: integração com link do deploy do front-end

--- src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/security/SecurityConfig.java
@@ -103,7 +103,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // Porta do seu React
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://metro-acesso-frontend.firebaseapp.com/**")); // Porta do seu React
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
