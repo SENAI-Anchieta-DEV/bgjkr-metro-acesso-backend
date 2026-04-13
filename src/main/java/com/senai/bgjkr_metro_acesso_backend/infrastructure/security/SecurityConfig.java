df2a87e5e14bdb15b6bf50fda317019140cb8ec3
Gabriel N. Mazzieri <101736362+GabrielMazzieri@users.noreply.github.com>
2026-04-13 10:40:53 -0300

    BGJKR-315 feat: Implementar conexão entre API e Frontend (#12)
    * BGJKR-176 Implementação de integração front-end CorsConfig
    
    * BGJKR-315 feat: integração com link do deploy do front-end
    
    * Merge remote-tracking branch 'origin/main' into BGJKR-176-Integracao-front-end

--- src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/security/SecurityConfig.java
@@ -12,6 +12,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -21,6 +27,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -90,5 +98,19 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://metro-acesso-frontend.firebaseapp.com/**")); // Porta do seu React
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
