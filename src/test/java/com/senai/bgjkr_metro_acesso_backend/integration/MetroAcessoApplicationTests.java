225c205fd2140b0917001a28f392a609234f4dfb
Brayan Laines Carvalho <brayanlainescarvalho@gmail.com>
2026-04-27 11:10:31 -0300

    BGJKR-325 feat: estrutura de testes de software

--- src/test/java/com/senai/bgjkr_metro_acesso_backend/integration/MetroAcessoApplicationTests.java
+++ src/test/java/com/senai/bgjkr_metro_acesso_backend/integration/MetroAcessoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.senai.bgjkr_metro_acesso_backend;
+package java.com.senai.bgjkr_metro_acesso_backend.integration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
