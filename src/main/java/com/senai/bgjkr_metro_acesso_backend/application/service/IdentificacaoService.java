8bae32c0bb4c52f6c3e8dbed6c37b89f5440ac8f
Brayan Laines Carvalho <brayanlainescarvalho@gmail.com>
2026-05-13 08:21:16 -0300

    BGJKR-144 refactor: remoção de classes sem uso e alteração de "evento" para "identificação"

--- src/main/java/com/senai/bgjkr_metro_acesso_backend/application/service/IdentificacaoService.java
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/application/service/IdentificacaoService.java
@@ -1,18 +1,12 @@
 package com.senai.bgjkr_metro_acesso_backend.application.service;
 
+import com.senai.bgjkr_metro_acesso_backend.application.dto.pendencia_atendimento.IdentificacaoDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IdentificacaoService {
-    private String ultimoEvento;
-
-    public String processarEvento(String payload) {
-        System.out.println("Processando evento...");
-        ultimoEvento = "Tag detectada: " + payload;
-        return ("Evento registrado: " + ultimoEvento);
-    }
-
-    public String getUltimoEvento() {
-        return ultimoEvento;
+    public void processarEvento(IdentificacaoDto evento) {
+        System.out.println("Evento recebido no service:");
+        System.out.println(evento.codigoTag());
     }
 }
--- src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/mqtt/MqttSubscriber.java
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/mqtt/MqttSubscriber.java
@@ -2,25 +2,22 @@ package com.senai.bgjkr_metro_acesso_backend.infrastructure.mqtt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.senai.bgjkr_metro_acesso_backend.application.dto.pendencia_atendimento.IdentificacaoDto;
-import com.senai.bgjkr_metro_acesso_backend.application.service.TagEventoService;
+import com.senai.bgjkr_metro_acesso_backend.application.service.IdentificacaoService;
 import jakarta.annotation.PostConstruct;
-import org.eclipse.paho.client.mqttv3.*;
+import lombok.AllArgsConstructor;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class MqttSubscriber {
-
-    private final TagEventoService service;
+    private final IdentificacaoService service;
     private final ObjectMapper objectMapper = new ObjectMapper();
-
-    public MqttSubscriber(TagEventoService service) {
-        this.service = service;
-    }
 
     @PostConstruct
     public void init() {
         try {
-
             MqttClient client = new MqttClient(
                     "tcp://localhost:1883",
                     MqttClient.generateClientId()
@@ -35,7 +32,6 @@ public class MqttSubscriber {
             System.out.println(" MQTT conectado (Render)");
 
             client.subscribe("metro/tag/entrada", (topic, msg) -> {
-
                 String payload = new String(msg.getPayload());
 
                 System.out.println(" MQTT recebido:");
@@ -49,7 +45,6 @@ public class MqttSubscriber {
                     e.printStackTrace();
                 }
             });
-
         } catch (Exception e) {
             System.out.println(" Erro MQTT");
             e.printStackTrace();
