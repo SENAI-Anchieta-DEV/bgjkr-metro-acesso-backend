52dbbc554839434cb0d6cf1641cf5dc1cab02412
Brayan Laines Carvalho <brayanlainescarvalho@gmail.com>
2026-05-13 09:32:27 -0300

    BGJKR-144 refactor: alteração

--- src/main/java/com/senai/bgjkr_metro_acesso_backend/application/service/IdentificacaoService.java
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/application/service/IdentificacaoService.java
@@ -1,12 +1,18 @@
 package com.senai.bgjkr_metro_acesso_backend.application.service;
 
 import com.senai.bgjkr_metro_acesso_backend.application.dto.pendencia_atendimento.IdentificacaoDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class IdentificacaoService {
-    public void processarEvento(IdentificacaoDto evento) {
+    private final PendenciaService pendenciaService;
+
+    public void processarEvento(IdentificacaoDto identificacao) {
         System.out.println("Evento recebido no service:");
-        System.out.println(evento.codigoTag());
+        System.out.println(identificacao.codigoTag());
+
+        pendenciaService.criarPendencia(identificacao);
     }
 }
--- src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/mqtt/MqttSubscriber.java
+++ src/main/java/com/senai/bgjkr_metro_acesso_backend/infrastructure/mqtt/MqttSubscriber.java
@@ -1,19 +1,28 @@
 package com.senai.bgjkr_metro_acesso_backend.infrastructure.mqtt;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.senai.bgjkr_metro_acesso_backend.application.dto.pendencia_atendimento.IdentificacaoDto;
 import com.senai.bgjkr_metro_acesso_backend.application.service.IdentificacaoService;
 import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @AllArgsConstructor
+@Slf4j // log padronizado do lombok
 public class MqttSubscriber {
     private final IdentificacaoService service;
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
 
     @PostConstruct
     public void init() {
@@ -29,25 +38,39 @@ public class MqttSubscriber {
 
             client.connect(options);
 
-            System.out.println(" MQTT conectado (Render)");
+            log.info("MQTT conectado");
 
             client.subscribe("metro/tag/entrada", (topic, msg) -> {
                 String payload = new String(msg.getPayload());
 
-                System.out.println(" MQTT recebido:");
-                System.out.println(payload);
+                log.info("MQTT recebido: {}", payload);
 
                 try {
                     IdentificacaoDto identificacao = objectMapper.readValue(payload, IdentificacaoDto.class);
+
+                    Authentication auth =
+                            new UsernamePasswordAuthenticationToken(
+                                    "mqtt-system",
+                                    null,
+                                    List.of(new SimpleGrantedAuthority(
+                                            "ROLE_ADMINISTRADOR"
+                                    ))
+                            );
+
+                    SecurityContextHolder.getContext()
+                            .setAuthentication(auth);
+
                     service.processarEvento(identificacao);
+                } catch (JsonProcessingException e) {
+                    log.error("Erro ao converter JSON MQTT", e);
                 } catch (Exception e) {
-                    System.out.println(" Erro ao converter JSON");
-                    e.printStackTrace();
+                    log.error("Erro processando evento MQTT", e);
+                } finally {
+                    SecurityContextHolder.clearContext();
                 }
             });
         } catch (Exception e) {
-            System.out.println(" Erro MQTT");
-            e.printStackTrace();
+            log.error("Erro ao iniciar MQTT", e);
         }
     }
 }
